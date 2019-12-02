21926045747384ffd13e071f83ca4660960393d9
tiagojosebogoni <tiagojosebogoni@gmail.com>
2019-12-02 18:41:02 -0300

    começado

---
+++ web/src/routes/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Switch}  from 'react-router-dom'
+
+import SingIn from '../pages/SignIn'
+
+
+export default function Routes(){
+  return(
+    <Switch>
+
+    </Switch>
+  )
+}
