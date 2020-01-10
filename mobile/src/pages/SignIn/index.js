0f115a4ad9a6dbe0310e194f7389ac7b4d9f7ff3
Tiago Bogoni <tiagojosebogoni@gmail.com>
2020-01-09 21:00:14 -0300

    Configurando a base do projeto

---
+++ mobile/src/pages/SignIn/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <Text>mundo</Text>
+  );
+}
