44743ab9b85dae2f75b47a6e7e419ac3bcb0c9f9
Tiago Bogoni <tiagojosebogoni@gmail.com>
2020-01-23 23:11:06 -0300

    Começado a desenvolver o componente de paginação

---
+++ web/src/components/Pagination/index.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+import { Container } from './styles';
+
+export default function Pagination({ load }) {
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    load('', page);
+  }, [page]); //eslint-disable-line
+
+  return (
+    <Container>
+      <button
+        disabled={page <= 1}
+        onClick={() => {
+          setPage(page - 1);
+        }}
+      >
+        <MdKeyboardArrowLeft size={40} />
+      </button>
+      <strong>{page}</strong>
+      <button
+        onClick={() => {
+          setPage(page + 1);
+          load('', page);
+        }}
+      >
+        <MdKeyboardArrowRight size={40} />
+      </button>
+    </Container>
+  );
+}
+
+Pagination.propTypes = {
+  load: PropTypes.func.isRequired,
+};
---
+++ web/src/components/Pagination/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  button {
+    border: 0;
+    background: #fff;
+  }
+
+  strong {
+    font-size: 18px;
+    margin: 0 20px 0 20px;
+  }
+`;
