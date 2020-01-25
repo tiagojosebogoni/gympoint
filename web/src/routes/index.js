7d6002c991ec15b16f0bc1da9e6599306be565f1
Tiago Bogoni <tiagojosebogoni@gmail.com>
2020-01-24 23:05:29 -0300

    Corrigido rota para cadastro de matrículas

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -24,7 +24,7 @@ export default function Routes() {
       <Route path="/plan/store" isPrivate component={StorePlan} />
 
       <Route path="/register/list" isPrivate component={RegisterList} />
-      <Route path="/register/list" isPrivate component={RegisterStore} />
+      <Route path="/register/store" isPrivate component={RegisterStore} />
 
       <Route path="/helpOrder" isPrivate component={HelpOrder} />
       <Route path="/" component={() => <h1>404</h1>} />
